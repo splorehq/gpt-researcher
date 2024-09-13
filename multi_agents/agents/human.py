bd054d6e08f29a40f623f041aee17df815ffc65b
Rohitash Hari Tanwar <rohitash.t@splore.com>
2024-09-13 15:33:30 +0530

    update toc format from string to array of strings

--- multi_agents/agents/human.py
+++ multi_agents/agents/human.py
@@ -13,39 +13,25 @@ class HumanAgent:
         task = research_state.get("task")
         layout = research_state.get("sections")
 
-        user_feedback = None
-
+        user_feedback = layout
+        
         if task.get("include_human_feedback"):
-            # Stream response to the user if a websocket is provided (such as from web app)
+            # Stream response to the user if a websocket is provided
             if self.websocket and self.stream_output:
                 try:
-                    await self.stream_output(
-                        "human_feedback",
-                        "request",
-                        f"Any feedback on this plan of topics to research? {layout}? If not, please reply with 'no'.",
-                        self.websocket,
-                    )
+                    await self.stream_output("human_feedback", "request", layout, self.websocket)
                     response = await self.websocket.receive_text()
-                    print(f"Received response: {response}", flush=True)
-                    response_data = json.loads(response)
-                    if response_data.get("type") == "human_feedback":
-                        user_feedback = response_data.get("value")
-                    else:
-                        print(
-                            f"Unexpected response type: {response_data.get('type')}",
-                            flush=True,
-                        )
+                    response = json.loads(response)
+                    # response = {
+                    #     "type": 'human_feedback',
+                    #     "value": layout
+                    # }
+                    # print(f"Received response: {response["value"]}")  # Add this line for debugging
+                    user_feedback = response["value"]
                 except Exception as e:
-                    print(f"Error receiving human feedback: {e}", flush=True)
+                    print(f"Error receiving human feedback: {e}")
             # Otherwise, prompt the user for feedback in the console
             else:
-                user_feedback = input(
-                    f"Any feedback on this plan? {layout}? If not, please reply with 'no'.\n>> "
-                )
+                user_feedback = input(f"Any feedback on this plan? {layout}? If not, please reply with 'no'.\n>> ")
 
-        if user_feedback and "no" in user_feedback.strip().lower():
-            user_feedback = None
-
-        print(f"User feedback before return: {user_feedback}")
-
-        return {"human_feedback": user_feedback}
+        return {"sections": user_feedback}
